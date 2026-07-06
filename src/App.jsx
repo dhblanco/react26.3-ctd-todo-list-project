4600648d8121229b3990bfa8fcc47eae9244719b
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-07-06 13:35:54 -0400

    add first todos using an array named todoList containing 3 empty objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 
 function App() {
 
+  const todoList = [
+    {id: 1, title: ""},
+    {id: 2, title: ""},
+    {id: 3, title: ""},
+  ]
+
   return (
     <div>
       <h1>Daniela's Todo List</h1>
+      <ul>
+        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        </ul>
     </div>
   )
 }
