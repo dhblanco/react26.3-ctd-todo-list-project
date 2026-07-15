fd4e1c705c17e8458b1732d5c66965abf0b765f4
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-07-14 23:02:52 -0400

    create TodoForm component file, build form structure, integrate TodoForm into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
 
   return (
     <div>
       <h1>Daniela's Todo List</h1>
+      <TodoForm />
       <TodoList />
     </div>
   )
