8f949ea4e7b52f58ec859268e9d1bd29ea6e03fe
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-04 23:04:48 -0400

    corrected syntax and input type to checkbox

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -2,11 +2,10 @@ function TodoListItem ({todo,onCompleteTodo}) {
     return (
     <li>
         <input
-            type="checked"
+            type="checkbox"
             checked={todo.isCompleted}
             onChange={() => onCompleteTodo(todo.id)}
         />
         {todo.title}</li>
-};
-
+)};
 export default TodoListItem;
