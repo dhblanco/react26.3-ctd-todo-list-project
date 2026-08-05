d9d0ea6cd08a9d380a1ece534acc3d33a43216ee
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-04 21:41:04 -0400

    replace return statement with ternary logic

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,9 @@
 import TodoListItem from "./TodoListItem";
 
 const TodoList = ({todoList}) => {
-    return (
+    return todoList.length === 0 ? (
+      <p>Add todo above to get started</p>
+    ) : (
       <ul>
           { todoList.map((todo) => (
               < TodoListItem key={todo.id} todo={todo} />
