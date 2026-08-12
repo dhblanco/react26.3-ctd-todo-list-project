365aadc2a254b5098acd5b06bf4bc763494c5b0d
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-11 21:02:57 -0400

    Import useState in TodoListItem to toggle btwn todo display and edit

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
+
 function TodoListItem ({todo,onCompleteTodo}) {
+    const [isEditing, setIsEditing] = useState("");
+    
     return (
     <li>
-        <input
-            type="checkbox"
-            checked={todo.isCompleted}
-            onChange={() => onCompleteTodo(todo.id)}
-        />
-        {todo.title}</li>
+        <form>
+            {isEditing ? (
+                <TextInputWithLabel value={todo.title}/>
+            ) : ( 
+                <>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={todo.isCompleted}
+                            onChange={() => onCompleteTodo(todo.id)}
+                         />
+                    </label>
+                    <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+                </>
+            )}
+        </form>
+    </li>
 )};
+
 export default TodoListItem;
