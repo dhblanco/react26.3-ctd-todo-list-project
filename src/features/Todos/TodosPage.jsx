147f589f35cbe73db3f6cc6263374b31e300b7a8
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-18 22:38:15 -0400

    corrected updated import paths

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,6 +1,6 @@
-import TodoList from './features/Todos/TodoList/TodoList.jsx';
-import TodoForm from './features/Todos/TodoForm.jsx';
 import { useState } from 'react';
+import TodoList from './TodoList/TodoList';
+import TodoForm from './TodoForm';
 
 function TodosPage() {
       //todoList holds current state
