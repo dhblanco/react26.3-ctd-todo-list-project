c1477370dac2c50b12da95d5ecc756d653676da6
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-18 21:53:38 -0400

    Create the TodosPage Component and Extract all todo state and functions from App.jsx to that new component

---
+++ src/features/Todos/TodosPage.jsx
@@ -0,0 +1,57 @@
+import TodoList from './features/TodoList/TodoList.jsx';
+import TodoForm from './features/TodoForm.jsx';
+import { useState } from 'react';
+
+function TodosPage() {
+      //todoList holds current state
+  //setTodoList updates the new state when called
+  const [todoList, setTodoList] = useState([]);
+  
+  const addTodo = (todoTitle) => {
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle,
+      isCompleted: false,
+    };
+
+    //ask React to update todo list state
+    //pass React a callback function
+    //then React supplies most updated todo list as "previous"
+    setTodoList((previous) => [newTodo, ...previous]);
+  };
+
+  const completeTodo = (id) => {
+    //declare previous todo list as parameter
+    setTodoList((previous) => {
+      return previous.map( (todo) => {
+        if (todo.id === id) {
+          return {...todo, isCompleted: true};
+          } 
+        return todo;
+      })
+    });
+  };
+
+  const updateTodo = (editedTodo) => {
+    const updatedTodos = todoList.map((todo) => {
+        if (todo.id === editedTodo.id) {
+          return {...editedTodo};
+        }
+        return todo;
+    });
+    setTodoList(updatedTodos);
+  };
+  
+    return(
+        <div>
+            <TodoForm onAddTodo={addTodo} />
+            <TodoList 
+                todoList={todoList} 
+                onCompleteTodo={completeTodo} 
+                onUpdateTodo={updateTodo}
+            />
+        </div>
+    );
+}
+
+export default TodosPage;
