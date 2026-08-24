58ea70a18f35bd33cff40e6907498d1f46d278bc
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-08-23 21:29:44 -0400

    refactor: update App to render simple Header

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,7 @@ function App() {
   
   return (
     <div>
-      <Header 
-        token={token}
-        onSetToken={setToken}
-        onSetEmail={setEmail}
-      />
+      <Header />
       { token ? (
         <TodosPage token={token} />
         ) : (
