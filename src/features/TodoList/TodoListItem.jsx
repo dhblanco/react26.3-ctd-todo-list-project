db5a4f911de4f086c203ad480d050f54a8463a3d
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-09-11 12:41:04 -0400

    revised elementId duplication

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -36,7 +36,7 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
             value={workingTitle}
             onChange={handleEdit}
             ref={inputRef}
-            elementId="todoTitle"
+            elementId={`todoTitle-${todo.id}`}
             labelText="Todo"
           />
         ) : (
